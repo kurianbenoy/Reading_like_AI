8c080dec9df9eb332b33c8526893e64576e03568
Kurian Benoy <kurian.bkk@gmail.com>
2020-11-15 21:30:15 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=["num2words"],
     entry_points={
         "console_scripts": [
-            "realpython=nosai.__main__:main",
+            "numbersai=nosai.__main__:main",
         ]
     },
 )
